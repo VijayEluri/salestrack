bbde41ea4bdabf09f69473d2657728eb3a5490c7
langeran <langeran@aa29f732-8038-11de-80f0-2398a13caac8>
2010-01-07 13:48:13 +0000

    Issue 5: functional test
    git-svn-id: https://salestrack.googlecode.com/svn/trade/trunk@22 aa29f732-8038-11de-80f0-2398a13caac8

--- dbreplicator/src/test/java/com/tort/replicator/SchemaUpdaterIT.java
+++ dbreplicator/src/test/java/com/tort/replicator/SchemaUpdaterIT.java
@@ -7,8 +7,8 @@ import static org.testng.Assert.*;
 
 import com.tort.replicator.SchemaUpdater;
 
-@Test
-public class SchemaUpdaterTest {
+@Test(groups = {"integration"})
+public class SchemaUpdaterIT {
 	public void schemaUpdate() throws IOException{
 		assertNotNull(this.getClass().getClassLoader().getResourceAsStream("trade.script"));		
 		new SchemaUpdater().updateSchema(this.getClass().getClassLoader().getResource("trade.script").getFile());
