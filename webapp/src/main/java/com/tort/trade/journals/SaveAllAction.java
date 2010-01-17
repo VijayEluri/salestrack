c5a2eab9d42c66713990c40902bf8e7c0d15c992
langeran <langeran@aa29f732-8038-11de-80f0-2398a13caac8>
2010-01-17 20:51:05 +0000

    refactoring
    git-svn-id: https://salestrack.googlecode.com/svn/trade/trunk@33 aa29f732-8038-11de-80f0-2398a13caac8

---
+++ webapp/src/main/java/com/tort/trade/journals/SaveAllAction.java
@@ -0,0 +1,34 @@
+package com.tort.trade.journals;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class SaveAllAction {
+
+	private List<TransitionTO> _transitions;
+
+	public SaveAllAction(Map<String, String[]> params) {
+		if(params.get("data") == null)
+			throw new IllegalArgumentException();
+			
+		String encodedTransitions = params.get("data")[0];		
+		
+		Type listType = new TypeToken<List<TransitionTO>>() {}.getType();		
+		_transitions = new Gson().fromJson(encodedTransitions, listType);
+	}
+
+	public byte[] act() {
+		ArrayList<Long> saved = new ArrayList<Long>();
+		for (TransitionTO transition : _transitions) {
+			saved.add(transition.getLid());
+		}
+				
+		return new Gson().toJson(saved).getBytes();
+	}
+
+}
