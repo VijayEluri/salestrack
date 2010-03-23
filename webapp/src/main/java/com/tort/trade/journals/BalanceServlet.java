1970524809d4e352d3da34f4b1eb5ce0b9338c3a
langeran <langeran@aa29f732-8038-11de-80f0-2398a13caac8>
2010-03-23 06:12:53 +0000

    Issue 14: page proto & servlet proto
    git-svn-id: https://salestrack.googlecode.com/svn/trade/trunk@64 aa29f732-8038-11de-80f0-2398a13caac8

---
+++ webapp/src/main/java/com/tort/trade/journals/BalanceServlet.java
@@ -0,0 +1,14 @@
+package com.tort.trade.journals;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BalanceServlet  extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException{
+				
+	}
+}
