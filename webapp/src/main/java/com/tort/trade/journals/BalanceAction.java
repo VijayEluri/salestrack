c82bdeb87a13d73af97d56ec8a1f5b0bc55a6e8b
langeran <langeran@aa29f732-8038-11de-80f0-2398a13caac8>
2010-04-02 08:15:30 +0000

    improve coverage
    git-svn-id: https://salestrack.googlecode.com/svn/trade/trunk@70 aa29f732-8038-11de-80f0-2398a13caac8

--- webapp/src/main/java/com/tort/trade/journals/BalanceAction.java
+++ webapp/src/main/java/com/tort/trade/journals/BalanceAction.java
@@ -1,6 +1,7 @@
 package com.tort.trade.journals;
 
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -9,10 +10,27 @@ public class BalanceAction implements Action<List<GoodBalance>> {
 
 	private final Session _session;
 	private final JournalQueryFactory _queryFactory;
+	private final Map _params;
 
-	public BalanceAction(Session session, JournalQueryFactory queryFactory) {
+	public BalanceAction(Session session, JournalQueryFactory queryFactory, Map params) {
+		if(session == null)
+			throw new IllegalArgumentException("session is null");
+		
+		if(queryFactory == null)
+			throw new IllegalArgumentException("query factory is null");
+		
+		if(params == null)
+			throw new IllegalArgumentException("params is null");
+		
+		if(params.size() == 0)
+			throw new IllegalArgumentException("params is empty");
+		
+		if(params.get("me") == null)
+			throw new IllegalArgumentException("me is null");
+		
 		_session = session;
 		_queryFactory = queryFactory;
+		_params = params;
 	}
 
 	@Override
--- webapp/src/test/java/com/tort/trade/journals/BalanceActionTest.java
+++ webapp/src/test/java/com/tort/trade/journals/BalanceActionTest.java
@@ -1,8 +1,11 @@
 package com.tort.trade.journals;
 
 import static org.easymock.EasyMock.*;
+import static org.testng.AssertJUnit.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -13,16 +16,87 @@ public class BalanceActionTest extends ActionTest {
 
 	@Override
 	protected Action positiveSetUp() {
-		Session session = createMock(Session.class);
+		return new BalanceAction(createSession(), createQueryFactory(), createParams());
+	}
+
+	private Session createSession() {
 		Query query = createMock(Query.class);
-		JournalQueryFactory queryFactory = createMock(JournalQueryFactory.class);
-		
 		expect(query.list()).andReturn(new ArrayList<GoodBalance>());
+		replay(query);
+		
+		Session session = createMock(Session.class);
 		expect(session.createQuery(isA(String.class))).andReturn(query);
+		replay(session);
+		return session;
+	}
+
+	private Map createParams() {
+		Map<String, String[]> params = new HashMap<String, String[]>();
+		params.put("me", new String[]{"1"});
+		
+		return params;
+	}
+
+	private JournalQueryFactory createQueryFactory() {
+		JournalQueryFactory queryFactory = createMock(JournalQueryFactory.class);
 		expect(queryFactory.getBalanceQuery()).andReturn("query");
+		replay(queryFactory);
 		
-		replay(session, query, queryFactory);
-		
-		return new BalanceAction(session, queryFactory);
+		return queryFactory;
+	}
+	
+	@Test
+	public void newBalanceActionNullSession(){
+		try{
+			new BalanceAction(null, createQueryFactory(), createParams());
+			fail();
+		} catch (IllegalArgumentException e){
+			
+		}
+	}
+	
+	@Test
+	public void newBalanceActionNullQueryFactory(){
+		try{
+			new BalanceAction(createSession(), null, createParams());
+			fail();
+		} catch (IllegalArgumentException e){
+			
+		}
+	}
+	
+	@Test
+	public void newBalanceActionNullParams(){
+		try {
+			new BalanceAction(createSession(), createQueryFactory(), null);
+			fail();
+		} catch (IllegalArgumentException e) {
+			
+		}
+	}
+	
+	@Test
+	public void newBalanceActionEmptyParams(){
+		try {
+			Map params = new HashMap();
+			new BalanceAction(createSession(), createQueryFactory(), params);
+			
+			fail();
+		} catch(IllegalArgumentException e){
+			
+		}
+	}
+	
+	@Test
+	public void newBalanceActionNullMeParam(){
+		try{
+			Map<String, String[]> params = new HashMap<String, String[]>();
+			params.put("unknown", new String[]{"1"});
+			
+			new BalanceAction(createSession(), createQueryFactory(), params);
+			fail();
+		} catch (IllegalArgumentException e) {
+			
+		}
 	}
 }
