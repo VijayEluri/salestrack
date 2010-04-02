82b2a187947310179dd73c6e7087d91b6c43bab7
langeran <langeran@aa29f732-8038-11de-80f0-2398a13caac8>
2010-04-02 08:16:12 +0000

    Issue 14: functional test
    git-svn-id: https://salestrack.googlecode.com/svn/trade/trunk@74 aa29f732-8038-11de-80f0-2398a13caac8

--- webapp/src/test/java/com/tort/trade/journals/GoodsBalanceIT.java
+++ webapp/src/test/java/com/tort/trade/journals/GoodsBalanceIT.java
@@ -5,11 +5,11 @@ import static org.testng.AssertJUnit.*;
 
 @Test(groups = {"functional"})
 public class GoodsBalanceIT extends FunctionalTest {
-	public void getBalance(){
-		_selenium.open("/webapp/journal.html");
-		_selenium.click("a[@name='balance']");
+	public void getBalance() throws InterruptedException{
+		_selenium.open("/webapp/journal");
+		_selenium.click("//a[@name='balance']");
 		
-		_selenium.waitForPageToLoad("5000");
+		waitForElement("//table[@id='balance']//tr[5]");
 		
 		assertEquals("Goods balance", _selenium.getTitle());
 	}
