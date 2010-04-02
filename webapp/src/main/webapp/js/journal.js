93666bda8c989e281f87b9bbfdef0ab2cfedbc8f
langeran <langeran@aa29f732-8038-11de-80f0-2398a13caac8>
2010-04-02 08:16:19 +0000

    minor refactoring
    git-svn-id: https://salestrack.googlecode.com/svn/trade/trunk@75 aa29f732-8038-11de-80f0-2398a13caac8

--- webapp/src/main/webapp/js/journal.js
+++ webapp/src/main/webapp/js/journal.js
@@ -16,7 +16,7 @@ journal = function() {
 		jQuery("td[class=priceItem]").click(function(){			
 			var goodName = jQuery(this).html();
 			var goodId = jQuery(this).attr("good_id");			
-			var me = jQuery("table[id=sales] > tbody > tr > td[class=active]").attr("name");
+			var me = menu.getMe();
 			addNewTransition(goodName, goodId, me);		
 			focusNewTransition();
 		});
