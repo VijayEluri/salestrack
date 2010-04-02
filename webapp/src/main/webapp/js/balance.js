3ed885b6e714f63cb6dcbb01b4e6757621060b8a
langeran <langeran@aa29f732-8038-11de-80f0-2398a13caac8>
2010-04-02 08:14:40 +0000

    Issue 14: balance table rendering
    git-svn-id: https://salestrack.googlecode.com/svn/trade/trunk@67 aa29f732-8038-11de-80f0-2398a13caac8

--- webapp/src/main/webapp/js/balance.js
+++ webapp/src/main/webapp/js/balance.js
@@ -1,9 +1,18 @@
 var balance = function(){
 	var menu;
+	var goodBalances = [{name: "jeans", balance: "10"}, {name: "shirt", balance: "5"}];
+	
+	var updateBalance = function (){
+		jQuery("#balance > tbody").html("");
+		jQuery.each(goodBalances, function(){
+			jQuery("#balance > tbody").append("<tr><td>" + this.name + "</td><td>" + this.balance + "</td></tr>");
+		});
+	}
 	
 	return {
 		init: function (activeMenuId){
 			menu = new Menu(activeMenuId);
+			updateBalance();
 		}
 	}
 }();
