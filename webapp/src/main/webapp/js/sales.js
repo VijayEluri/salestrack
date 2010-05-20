c6a04a1a5306a0e9ea2b6b0d8717905e00415c08
tort <tort@tort-m.(none)>
2010-05-20 11:04:34 +0400

    cancel sale edit

--- webapp/src/main/webapp/js/sales.js
+++ webapp/src/main/webapp/js/sales.js
@@ -1,29 +1,37 @@
 var editSales = function(){
     var mainMenu;
     var sales = [{name: "Саша", alias: "С"}, {name: "Оля", alias: "О"}];
+    var name;
+    var alias;
 
     var editSale = function(i) {
-        var name = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html();
+        name = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html();
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html("<input style='width: 9em;' value='" + name + "'/>");
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name] > input").focus();
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name] > input").select();
-        var alias = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html();
+        alias = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html();
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html("<input style='width: 9em;' value='" + alias + "'/>");
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.commit(" + i + ");'>Принять</a>&nbsp;|&nbsp;<a href='#'>Отменить</a>");
+        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.commit(" + i + ");'>Принять</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.cancel(" + i + ")'>Отменить</a>");
     };
 
-    var commitSale = function(i) {
+    var commitEdit = function(i) {
         var name = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name] > input").val();
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html(name);
-        var name = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias] > input").val();
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html(name);
+        var alias = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias] > input").val();
+        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html(alias);
+        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>");
+    };
+
+    var cancelEdit = function(i){
+        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html(name);
+        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html(alias);
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>");
     };
 
     var render = function() {
         jQuery("#sales > tbody > tr:has(td)").remove();
         jQuery.each(sales, function(i, sale) {
-            jQuery("#sales > tbody").append("<tr index='" + i + "'><td>" + i + "</td><td name='name'>" + sale.name + "</td><td name='alias'>" + sale.alias + "</td><td name='link'><a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a></td></tr>");
+            jQuery("#sales > tbody").append("<tr index='" + i + "'><td style='height: 29px;'>" + i + "</td><td name='name'>" + sale.name + "</td><td name='alias'>" + sale.alias + "</td><td name='link'><a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a></td></tr>");
         });
     };
 
@@ -37,7 +45,10 @@ var editSales = function(){
             editSale(i);
         },
         commit: function(i){
-            commitSale(i);
+            commitEdit(i);
+        },
+        cancel: function(i){
+            cancelEdit(i);
         }
     }
 }();
