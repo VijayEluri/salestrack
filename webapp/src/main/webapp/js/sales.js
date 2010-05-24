a09062c9d58d3ab36c3ee8a13bdc844cad8b2a50
tort <langeran@gmail.com>
2010-05-24 10:15:16 +0400

    refactor rendering and operations

--- webapp/src/main/webapp/js/sales.js
+++ webapp/src/main/webapp/js/sales.js
@@ -1,17 +1,18 @@
-var editSales = function(){
+var editSales = function() {
     var mainMenu;
-    var sales = [{name: "Саша", alias: "С"}, {name: "Оля", alias: "О"}];
+    var sales = [
+            new Sale("Оля", "О"),
+            new Sale("Саша", "С")
+    ];
     var name;
     var alias;
 
     var editSale = function(i) {
-        name = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html();
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html("<input style='width: 9em;' value='" + name + "'/>");
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name] > input").focus();
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name] > input").select();
-        alias = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html();
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html("<input style='width: 9em;' value='" + alias + "'/>");
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.commit(" + i + ");'>Принять</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.cancel(" + i + ")'>Отменить</a>");
+        var sale = sales[i];
+        sale.edit();
+        renderSale(i);
+        jQuery("input[focus]").focus();
+        jQuery("input[focus]").select();
     };
 
     var commitEdit = function(i) {
@@ -22,24 +23,28 @@ var editSales = function(){
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.remove(" + i + ")'>Удалить</a>");
     };
 
-    var cancelEdit = function(i){
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html(name);
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html(alias);
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.remove(" + i + ")'>Удалить</a>");
-        if(name == "" && alias == ""){
-            removeSale(i);
-        }
+    var cancelEdit = function(i) {
+        var sale = sales[i];
+        sale.cancel();
+        renderSale(i);
     };
 
     var render = function() {
         jQuery("#sales > tbody > tr:has(td)").remove();
         jQuery.each(sales, function(i, sale) {
-            appendSale(i, sale);
+            appendSale(i);
         });
     };
 
-    var appendSale = function(i, sale) {
-        jQuery("#sales > tbody").append("<tr index='" + i + "'><td style='height: 29px;'>" + i + "</td><td name='name'>" + sale.name + "</td><td name='alias'>" + sale.alias + "</td><td name='link'><a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.remove(" + i + ")'>Удалить</a></td></tr>");
+    var appendSale = function(i) {
+        var sale = sales[i];
+        jQuery("#sales > tbody").append("<tr index='" + i + "'></tr>");
+        renderSale(i);
+    };
+
+    var renderSale = function(i) {
+        var sale = sales[i];
+        jQuery("#sales > tbody > tr[index=" + i + "]").html(sale.render(i));
     };
 
     var addSale = function() {
@@ -50,31 +55,89 @@ var editSales = function(){
     };
 
     var removeSale = function(i) {
-        if (confirm("Подтвердите удаление")){
+        if (confirm("Подтвердите удаление")) {
             jQuery("#sales > tbody > tr[index=" + i + "]").remove();
         }
     };
 
     return {
-        init: function(menuItem){
+        init: function(menuItem) {
             mainMenu = new SubMenu('#mainMenu', 'mainMenu.html');
             mainMenu.init(menuItem);
             render();
         },
-        edit: function(i){
+        edit: function(i) {
             editSale(i);
         },
-        add: function(){
+        add: function() {
             addSale();
         },
-        commit: function(i){
+        commit: function(i) {
             commitEdit(i);
         },
-        cancel: function(i){
+        cancel: function(i) {
             cancelEdit(i);
         },
-        remove: function(i){
+        remove: function(i) {
             removeSale(i);
         }
     }
 }();
+
+function Sale(name, alias) {
+    var i;
+    var name = name;
+    var alias = alias;
+    var state = new ViewState();
+
+    return {
+        render: function(newI) {
+            i = newI;
+            return state.render(i, name, alias);
+        },
+        getName: function(){
+            return name;
+        },
+        getAlias: function(){
+            return alias;
+        },
+        setName: function(newName){
+            name = newName;
+        },
+        setAlias: function(newAlias){
+            alias = newAlias;
+        },
+        edit: function(){
+            state = new EditState();
+        },
+        view: function(){
+            state = new ViewState();
+        },
+        cancel: function(){
+            state = new ViewState();
+        }
+    }
+}
+
+function ViewState() {
+    return {
+        render: function(i, name, alias) {
+                            return "<td style='height: 29px;'>" + i + "</td>" +
+                            "<td name='name'>" + name + "</td>" +
+                            "<td name='alias'>" + alias + "</td>" +
+                            "<td name='link'><a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.remove(" + i + ")'>Удалить</a></td>"; 
+        }
+    }
+}
+
+function EditState() {
+    return {
+        render: function(i, name, alias) {
+                            return "<td style='height: 29px;'>" + i + "</td>" +
+                            "<td name='name'><input focus style='width: 9em;' value='" + name + "'/></td>" +
+                            "<td name='alias'><input style='width: 9em;' value='" + alias + "'/></td>" +
+                            "<td name='link'><a href='#' onclick='editSales.commit(" + i + ");'>Принять</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.cancel(" + i + ")'>Отменить</a></td>";
+        }
+    }
+}
+
