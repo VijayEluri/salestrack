0d72cc2c082441cc76ca09317ac1abf0ff8aa21c
tort <langeran@gmail.com>
2010-05-26 01:20:08 +0400

    add sales, remove sales

--- webapp/src/main/webapp/js/sales.js
+++ webapp/src/main/webapp/js/sales.js
@@ -19,20 +19,40 @@ var editSales = function(){
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html(name);
         var alias = jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias] > input").val();
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html(alias);
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>");
+        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.remove(" + i + ")'>Удалить</a>");
     };
 
     var cancelEdit = function(i){
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=name]").html(name);
         jQuery("#sales > tbody > tr[index=" + i + "] > td[name=alias]").html(alias);
-        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>");
+        jQuery("#sales > tbody > tr[index=" + i + "] > td[name=link]").html("<a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.remove(" + i + ")'>Удалить</a>");
+        if(name == "" && alias == ""){
+            removeSale(i);
+        }
     };
 
     var render = function() {
         jQuery("#sales > tbody > tr:has(td)").remove();
         jQuery.each(sales, function(i, sale) {
-            jQuery("#sales > tbody").append("<tr index='" + i + "'><td style='height: 29px;'>" + i + "</td><td name='name'>" + sale.name + "</td><td name='alias'>" + sale.alias + "</td><td name='link'><a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a></td></tr>");
+            appendSale(i, sale);
         });
+    };
+
+    var appendSale = function(i, sale) {
+        jQuery("#sales > tbody").append("<tr index='" + i + "'><td style='height: 29px;'>" + i + "</td><td name='name'>" + sale.name + "</td><td name='alias'>" + sale.alias + "</td><td name='link'><a href='#' onclick='editSales.edit(" + i + ");'>Изменить</a>&nbsp;|&nbsp;<a href='#' onclick='editSales.remove(" + i + ")'>Удалить</a></td></tr>");
+    };
+
+    var addSale = function() {
+        var length = sales.length;
+        sales.push({name: "", alias: ""});
+        appendSale(length, sales[length]);
+        editSale(length);
+    };
+
+    var removeSale = function(i) {
+        if (confirm("Подтвердите удаление")){
+            jQuery("#sales > tbody > tr[index=" + i + "]").remove();
+        }
     };
 
     return {
@@ -44,11 +64,17 @@ var editSales = function(){
         edit: function(i){
             editSale(i);
         },
+        add: function(){
+            addSale();
+        },
         commit: function(i){
             commitEdit(i);
         },
         cancel: function(i){
             cancelEdit(i);
+        },
+        remove: function(i){
+            removeSale(i);
         }
     }
 }();
