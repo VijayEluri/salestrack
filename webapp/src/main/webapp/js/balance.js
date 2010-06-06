17a14680a5c915bebcbecbe4021033257a26f744
tort <langeran@gmail.com>
2010-06-06 17:07:18 +0400

    clean code

--- webapp/src/main/webapp/js/balance.js
+++ webapp/src/main/webapp/js/balance.js
@@ -1,34 +1,21 @@
 var balance = function(){
 	var menu;
-	var goodBalances = [{name: "jeans", balance: "10"}, {name: "shirt", balance: "5"}];
 	
 	var updateBalance = function (me){
-		jQuery("#balance > tbody").empty();
-		requestBalance(me);
-	}
-	
-	var getGoodBalances = function(me){
-		if(me == 5){
-			return [{name: "hat", balance: "33"}, {name: "boots", balance: "55"}];
-		}
-		
-		return goodBalances;
-	}
-	
-	var requestBalance = function(me){
-		jQuery.ajax({
-			url: "balance",			
-			data: "me=" + menu.getMe(),
-			type: "GET",
-			contentType: "application/x-www-form-urlencoded; charset=utf-8",
-			dataType: "json",						
-			error: function(){jQuery("div[class=error]").text("Ошибка сохранения передач");},
-			success: function(balances){
-				jQuery.each(balances, function(){
-					jQuery("#balance > tbody").append("<tr><td>" + this._good._name + "</td><td>" + this._balance + "</td></tr>");
-				});
-			}
-		});
+        jQuery.ajax({
+            url: "balance",
+            data: "me=" + menu.getMe(),
+            type: "GET",
+            contentType: "application/x-www-form-urlencoded; charset=utf-8",
+            dataType: "json",
+            error: function(){alert("Сервер недоступен")},
+            success: function(balances){
+                jQuery("#balance > tbody").empty();
+                jQuery.each(balances, function(){
+                    jQuery("#balance > tbody").append("<tr><td>" + this._good._name + "</td><td>" + this._balance + "</td></tr>");
+                });
+            }
+        });
 	}
 	
 	return {
