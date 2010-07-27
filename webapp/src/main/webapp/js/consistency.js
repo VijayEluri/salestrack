e6fc598e03fc027cd098c17bb047fcd7d1fe5442
Alexey Nesterov <langeran@gmail.com>
2010-07-27 21:37:22 +0400

    reformat

--- webapp/src/main/webapp/js/consistency.js
+++ webapp/src/main/webapp/js/consistency.js
@@ -1,38 +1,47 @@
-var consistency = function(){
-    var diffs = [{_day: "01.01.2010", _errors: [{_me: 3, _good: "Джинсы синие", _diff: "+3В"}]},
-                 {_day: "02.01.2010", _errors: [{_me: 3, _good: "Джинсы красные", _diff: "-2С"},
-                                              {_me: 3, _good: "Джинсы зеленые", _diff: "+5В"}
-                                             ]
-                 }
-                ];
+var consistency = function() {
+    var diffs = [
+        {_day: "01.01.2010", _errors: [
+            {_me: 3, _good: "Джинсы синие", _diff: "+3В"}
+        ]},
+        {_day: "02.01.2010", _errors: [
+            {_me: 3, _good: "Джинсы красные", _diff: "-2С"},
+            {_me: 3, _good: "Джинсы зеленые", _diff: "+5В"}
+        ]
+        }
+    ];
     var menu;
 
-    var render = function(me, diffs){
+    var render = function(me, diffs) {
         jQuery("table#diffs > tbody").empty();
-        jQuery.each(diffs, function(i, diff){
+        jQuery.each(diffs, function(i, diff) {
             jQuery("table#diffs > tbody").append("<tr><td colspan='2'>" + diff._day + "</td></tr>");
-            jQuery.each(diff._errors, function(j, error){
-                if(error._me == me)
+            jQuery.each(diff._errors, function(j, error) {
+                if (error._me == me)
                     jQuery("table#diffs > tbody").append("<tr><td>" + error._good + "</td><td>" + error._diff + "</td></tr>");
             });
         });
     };
 
-    var loadDiffs = function(){
+    var loadDiffs = function() {
         jQuery.ajax({
-				url: "getConsistency",
-				data: "me=" + menu.getMe(),
-				type: "GET",
-				dataType: "json",
-				error: function(){alert("Сервер недоступен");},
-				success: function(data){diffs = data; render(menu.getMe(), diffs);}
-			});
+            url: "getConsistency",
+            data: "me=" + menu.getMe(),
+            type: "GET",
+            dataType: "json",
+            error: function() {
+                alert("Сервер недоступен");
+            },
+            success: function(data) {
+                diffs = data;
+                render(menu.getMe(), diffs);
+            }
+        });
     };
 
     return {
-        init: function(activeMenu){
+        init: function(activeMenu) {
             menu = new Menu("consistency", activeMenu);
-            menu.getSalesMenu().bind(function(){
+            menu.getSalesMenu().bind(function() {
                 loadDiffs();
             });
         }
