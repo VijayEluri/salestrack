e5fe86b206f364b98e8a549b34a6b091b34bd305
Alexey Nesterov <langeran@gmail.com>
2010-08-10 16:08:37 +0400

    reformat

--- webapp/src/main/java/com/tort/trade/journals/TransitionConversation.java
+++ webapp/src/main/java/com/tort/trade/journals/TransitionConversation.java
@@ -5,13 +5,13 @@ import org.hibernate.Session;
 import com.tort.trade.model.Sales;
 
 public class TransitionConversation {
-	private Session _hibernateSession;
-	
-	public Session getHibernateSession() {
-		return _hibernateSession;
-	}
+    private Session _hibernateSession;
 
-	public void setHibernateSession(Session hibernateSession) {
-		_hibernateSession = hibernateSession;
-	}
+    public Session getHibernateSession() {
+        return _hibernateSession;
+    }
+
+    public void setHibernateSession(Session hibernateSession) {
+        _hibernateSession = hibernateSession;
+    }
 }
