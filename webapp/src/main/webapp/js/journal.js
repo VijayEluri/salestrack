3dcabf50144ce9d24c43b2c138acd77b925cf0ea
Alexey Nesterov <langeran@gmail.com>
2010-08-17 11:56:48 +0400

    journals. add goods number calculation

--- webapp/src/main/webapp/js/journal.js
+++ webapp/src/main/webapp/js/journal.js
@@ -28,6 +28,11 @@ journal = function() {
         clearGoods();
         addGoods();
         setupGoodsEventHandlers();
+        updateGoodsCount(goods.length);
+    };
+
+    var updateGoodsCount = function (count){
+        jQuery("#goodCount").html(count);
     };
 
     var goodsFilter = function () {
