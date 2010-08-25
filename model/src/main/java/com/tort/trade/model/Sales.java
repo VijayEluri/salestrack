716919235d12704f1f5e6127b5730b3394ff5a54
Alexey Nesterov <langeran@gmail.com>
2010-08-25 22:06:15 +0400

    Revert "reformat"
    This reverts commit dc083fdb8e696fc5f581d67331f0e6fe40a705bc.

--- model/src/main/java/com/tort/trade/model/Sales.java
+++ model/src/main/java/com/tort/trade/model/Sales.java
@@ -10,38 +10,36 @@ import javax.persistence.*;
 @Table(name = "DEP")
 @SequenceGenerator(name = "sales_gen", initialValue = 777, allocationSize = 20, sequenceName = "sales_gen")
 public class Sales {
-    private Long _id;
-    private String _name;
+	private Long _id;
+	private String _name;
     private SalesAlias _alias;
 
-    public Sales() {
+    public Sales(){
 
     }
 
-    public Sales(String name) {
-        _name = name;
-    }
+	public Sales(String name) {
+		_name = name;
+	}
 
-    @Id
-    @Column(name = "DEP_SEQ")
+	@Id
+	@Column(name = "DEP_SEQ")
     @GeneratedValue(generator = "sales_gen")
-    public Long getId() {
-        return _id;
-    }
-
-    public void setId(Long id) {
-        _id = id;
-    }
+	public Long getId() {
+		return _id;
+	}
+	public void setId(Long id) {
+		_id = id;
+	}
 
     @NotNull
-    @Column(name = "DEP_NAME")
-    public String getName() {
-        return _name;
-    }
-
-    public void setName(String name) {
-        _name = name;
-    }
+	@Column(name = "DEP_NAME")
+	public String getName() {
+		return _name;
+	}
+	public void setName(String name) {
+		_name = name;
+	}
 
     @OneToOne(mappedBy = "sales")
     public SalesAlias getAlias() {
