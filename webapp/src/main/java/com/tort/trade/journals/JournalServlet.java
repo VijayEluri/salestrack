0ec17739da42087b8613b84cdd314cf40de61f88
Alexey Nesterov <langeran@gmail.com>
2010-08-25 22:05:39 +0400

    Revert "reformat"
    This reverts commit 91eac2047f66fe2b2e1c88e401d3ec5f3d2bc311.

--- webapp/src/main/java/com/tort/trade/journals/JournalServlet.java
+++ webapp/src/main/java/com/tort/trade/journals/JournalServlet.java
@@ -11,17 +11,17 @@ import org.hibernate.Session;
 
 import com.tort.trade.model.Sales;
 
-public class JournalServlet extends HttpServlet {
+public class JournalServlet  extends HttpServlet{
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Session session = new SessionFactoryUtil().getSessionFactory().openSession();
-
-        TransitionConversation conversation = new TransitionConversation();
-        conversation.setHibernateSession(session);
-        req.getSession().setAttribute(Constants.CONVERSATION, conversation);
-
-        resp.sendRedirect("journal.html");
-    }
-
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Session session = new SessionFactoryUtil().getSessionFactory().openSession();
+		
+		TransitionConversation conversation = new TransitionConversation();
+		conversation.setHibernateSession(session);
+		req.getSession().setAttribute(Constants.CONVERSATION, conversation);
+		
+		resp.sendRedirect("journal.html");
+	}
+	
 }
