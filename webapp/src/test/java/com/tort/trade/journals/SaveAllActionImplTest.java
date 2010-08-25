d0fb58ba6bcf2e9e5292847485392c042fd9556b
Alexey Nesterov <langeran@gmail.com>
2010-08-25 22:06:20 +0400

    Revert "reformat"
    This reverts commit 838f91ddd99d509d8e3e7bf41a2d393ee53bb4dd.

--- webapp/src/test/java/com/tort/trade/journals/SaveAllActionImplTest.java
+++ webapp/src/test/java/com/tort/trade/journals/SaveAllActionImplTest.java
@@ -15,140 +15,140 @@ import com.tort.trade.model.Transition;
 
 @Test
 public class SaveAllActionImplTest extends ActionTest {
+	
+	public void newActionNegativeNoDataParam(){
+		try{
+			Map<String, String[]> params = new HashMap<String, String[]>();
+			params.put("me", new String[]{"1"});
+			
+			new SaveAllAction(params, null, null);
+			fail();
+		} catch (IllegalArgumentException ignored) {
 
-    public void newActionNegativeNoDataParam() {
-        try {
-            Map<String, String[]> params = new HashMap<String, String[]>();
-            params.put("me", new String[]{"1"});
+		}
+	}
+	
+	public void newActionNegativeNoMeParam(){
+		try{
+			Map<String, String[]> params = new HashMap<String, String[]>();
+			params.put("data", new String[]{"[{_goodId: 1, _text: \"драссте вам\", _lid: 1}]"});
+			
+			new SaveAllAction(params, null, null);
+			fail();
+		} catch (IllegalArgumentException ignored) {
 
-            new SaveAllAction(params, null, null);
-            fail();
-        } catch (IllegalArgumentException ignored) {
+		}
+	}
+	
+	public void newActionNegativeZeroLengthMeParam(){
+		try{
+			Map<String, String[]> params = new HashMap<String, String[]>();
+			params.put("data", new String[]{"[{_goodId: 1, _text: \"драссте вам\", _lid: 1}]"});
+			params.put("me", new String[]{});
+			
+			new SaveAllAction(params, null, null);
+			fail();
+		} catch (IllegalArgumentException ignored) {
 
-        }
-    }
-
-    public void newActionNegativeNoMeParam() {
-        try {
-            Map<String, String[]> params = new HashMap<String, String[]>();
-            params.put("data", new String[]{"[{_goodId: 1, _text: \"драссте вам\", _lid: 1}]"});
-
-            new SaveAllAction(params, null, null);
-            fail();
-        } catch (IllegalArgumentException ignored) {
-
-        }
-    }
-
-    public void newActionNegativeZeroLengthMeParam() {
-        try {
-            Map<String, String[]> params = new HashMap<String, String[]>();
-            params.put("data", new String[]{"[{_goodId: 1, _text: \"драссте вам\", _lid: 1}]"});
-            params.put("me", new String[]{});
-
-            new SaveAllAction(params, null, null);
-            fail();
-        } catch (IllegalArgumentException ignored) {
-
-        }
-    }
-
-    public void newActionNullParams() {
-        try {
+		}
+	}
+	
+	public void newActionNullParams(){
+		try {
             final Session session = createMock(Session.class);
             TransitionConversation conversation = createMock(TransitionConversation.class);
             expect(conversation.getHibernateSession()).andReturn(session);
             replay(conversation);
 
             new SaveAllAction(null, conversation, createMock(TransitionConverterLookup.class));
-            fail();
-        } catch (IllegalArgumentException ignored) {
-
-        }
-    }
-
-    public void newActionNullSession() {
-        try {
-            new SaveAllAction(createParams(), null, createMock(TransitionConverterLookup.class));
-            fail();
-        } catch (IllegalArgumentException ignored) {
-
-        }
-    }
-
-    public void newActionNullLookup() {
-        try {
+			fail();
+		} catch (IllegalArgumentException ignored) {
+			
+		}
+	}
+	
+	public void newActionNullSession(){
+		try {
+			new SaveAllAction(createParams(), null, createMock(TransitionConverterLookup.class));
+			fail();
+		} catch (IllegalArgumentException ignored) {
+			
+		}
+	}
+	
+	public void newActionNullLookup(){
+		try{
             final Session session = createMock(Session.class);
             TransitionConversation conversation = createMock(TransitionConversation.class);
             expect(conversation.getHibernateSession()).andReturn(session);
             replay(conversation);
 
-            new SaveAllAction(createParams(), conversation, null);
-            fail();
-        } catch (IllegalArgumentException ignored) {
-
-        }
-    }
-
-    public void newActionPositive() {
-        Session session = createMock(Session.class);
-        expect(session.load(eq(Sales.class), isA(Long.class))).andReturn(new Sales("test"));
-        replay(session);
+			new SaveAllAction(createParams(), conversation, null);
+			fail();
+		} catch (IllegalArgumentException ignored) {
+			
+		}
+	}
+	
+	public void newActionPositive(){
+		Session session = createMock(Session.class);
+		expect(session.load(eq(Sales.class), isA(Long.class))).andReturn(new Sales("test"));
+		replay(session);
 
         TransitionConversation conversation = createMock(TransitionConversation.class);
         expect(conversation.getHibernateSession()).andReturn(session);
         replay(conversation);
 
-        new SaveAllAction(createParams(), conversation, createMock(TransitionConverterLookup.class));
-    }
+		new SaveAllAction(createParams(), conversation, createMock(TransitionConverterLookup.class));
+	}
 
-    private Map<String, String[]> createParams() {
-        Map<String, String[]> params = new HashMap<String, String[]>();
-        params.put("data", new String[]{"[{_goodId: 1, _text: \"драссте вам\", _lid: 1}]"});
-        params.put("me", new String[]{"1"});
-        return params;
-    }
-
-    public void actImpl() {
-        Action action = positiveSetUp();
-
-        View result = action.act();
-
-        assertNotNull(result);
-    }
-
-    @SuppressWarnings({"ThrowableInstanceNeverThrown"})
+	private Map<String, String[]> createParams() {
+		Map<String, String[]> params = new HashMap<String, String[]>();
+		params.put("data", new String[]{"[{_goodId: 1, _text: \"драссте вам\", _lid: 1}]"});
+		params.put("me", new String[]{"1"});
+		return params;
+	}
+	
+	public void actImpl(){
+		Action action = positiveSetUp();
+		
+		View result = action.act();		
+		
+		assertNotNull(result);
+	}
+	
+	@SuppressWarnings({"ThrowableInstanceNeverThrown"})
     protected Action positiveSetUp() {
-        TransitionConverter converter = createMock(TransitionConverter.class);
-        TransitionConverterLookup converterLookup = createMock(TransitionConverterLookup.class);
-
-        Map<String, String[]> params = new HashMap<String, String[]>();
-        params.put("data", new String[]{"[{_goodId: 5, _text: \"bad_data\", _lid: 5}, {_goodId: 1, _text: \"good_data\", _lid: 1}, {_goodId: 2, _text: \"bad_data\", _lid: 2}]"});
-        params.put("me", new String[]{"1"});
+		TransitionConverter converter = createMock(TransitionConverter.class);
+		TransitionConverterLookup converterLookup = createMock(TransitionConverterLookup.class);
+		
+		Map<String, String[]> params = new HashMap<String, String[]>();
+		params.put("data", new String[]{"[{_goodId: 5, _text: \"bad_data\", _lid: 5}, {_goodId: 1, _text: \"good_data\", _lid: 1}, {_goodId: 2, _text: \"bad_data\", _lid: 2}]"});
+		params.put("me", new String[]{"1"});
 
         final Session session = createMock(Session.class);
         TransitionConversation conversation = createMock(TransitionConversation.class);
         expect(conversation.getHibernateSession()).andReturn(session);
         replay(conversation);
 
-        SaveAllAction action = new SaveAllAction(params, conversation, converterLookup);
-
-        expect(session.load(eq(Sales.class), isA(Long.class))).andReturn(new Sales("test"));
-        expect(converterLookup.getTransitionConverter(isA(Session.class), isA(Sales.class))).andReturn(converter);
-        expect(session.save(isA(Transition.class))).andStubReturn(1L);
-        try {
-            expect(converter.convertToEntity(isA(TransitionTO.class))).andThrow(new ConvertTransitionException());
-            ArrayList<Transition> transitions = new ArrayList<Transition>();
-            transitions.add(new Transition());
-            expect(converter.convertToEntity(isA(TransitionTO.class))).andReturn(transitions);
-            expect(converter.convertToEntity(isA(TransitionTO.class))).andThrow(new ConvertTransitionException());
-        } catch (ConvertTransitionException e) {
-            e.printStackTrace();
-        }
-        session.flush();
-        replay(converter, converterLookup, session);
-
-        return action;
-    }
+		SaveAllAction action = new  SaveAllAction(params, conversation, converterLookup);
+		
+		expect(session.load(eq(Sales.class), isA(Long.class))).andReturn(new Sales("test"));
+		expect(converterLookup.getTransitionConverter(isA(Session.class), isA(Sales.class))).andReturn(converter);
+		expect(session.save(isA(Transition.class))).andStubReturn(1L);
+		try {
+			expect(converter.convertToEntity(isA(TransitionTO.class))).andThrow(new ConvertTransitionException());
+			ArrayList<Transition> transitions = new ArrayList<Transition>();
+			transitions.add(new Transition());
+			expect(converter.convertToEntity(isA(TransitionTO.class))).andReturn(transitions);
+			expect(converter.convertToEntity(isA(TransitionTO.class))).andThrow(new ConvertTransitionException());			
+		} catch (ConvertTransitionException e) {
+			e.printStackTrace();
+		}
+		session.flush();
+		replay(converter, converterLookup, session);
+		
+		return action;
+	}
 }
 
