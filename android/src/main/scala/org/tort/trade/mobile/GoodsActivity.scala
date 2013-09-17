faa78109e5a91b7c25a247cf55e5adfdda4bae76
tort <langeran@gmail.com>
2013-09-17 14:44:02 +0400

    rework ui for selecting goods

--- android/src/main/scala/org/tort/trade/mobile/GoodsActivity.scala
+++ android/src/main/scala/org/tort/trade/mobile/GoodsActivity.scala
@@ -2,15 +2,13 @@ package org.tort.trade.mobile
 
 import android.os.{AsyncTask, Bundle}
 import android.widget._
-import java.util
-import scala.collection.JavaConversions._
 import android.app.Activity
-import android.view.View
-import android.widget.AdapterView.OnItemClickListener
 import scala.slick.jdbc.{StaticQuery => Q}
 import Q.interpolation
 import scala.slick.session.Database
 import Database.threadLocalSession
+import android.view.View.OnClickListener
+import android.view.View
 
 class GoodsActivity extends TypedActivity {
   override def onCreate(savedInstanceState: Bundle) {
@@ -19,31 +17,12 @@ class GoodsActivity extends TypedActivity {
     setContentView(R.layout.goods)
 
     setDirectionText
-    val goodsListView = findViewById(R.id.goodsListView).asInstanceOf[ListView]
-    setGoodsList(context, goodsListView, "")
+    val goodsGrid = findViewById(R.id.goodsGridLayout).asInstanceOf[GridLayout]
+    loadGoods(goodsGrid, "")
   }
 
-
-  private def setGoodsList(context: GoodsActivity, goodsListView: ListView, subname: String) {
-    goodsListView.setOnItemClickListener(new OnItemClickListener() {
-      def onItemClick(parent: AdapterView[_], view: View, position: Int, id: Long) {
-        val layout = findViewById(R.id.goodsLayout).asInstanceOf[LinearLayout]
-        val listView: ListView = new ListView(context)
-        val layoutParams = new LinearLayout.LayoutParams(100, -1, 1F)
-        layout.addView(listView)
-        listView.setLayoutParams(layoutParams)
-        val textView = view.asInstanceOf[TextView]
-        println("SUBNAME: " + textView.getText.toString)
-        setGoodsList(context, listView, textView.getText.toString)
-      }
-    })
-
-    loadGoods(goodsListView, subname)
-  }
-
-
-  def loadGoods(goodsListView: ListView, subname: String) {
-    new GoodsTask(subname, this, goodsListView).execute()
+  def loadGoods(gridLayout: GridLayout, subname: String) {
+    new GoodsTask(subname, this, gridLayout).execute()
   }
 
   private def setDirectionText {
@@ -53,18 +32,25 @@ class GoodsActivity extends TypedActivity {
   }
 }
 
-class GoodsTask(subname: String, activity: Activity, goodsListView: ListView) extends AsyncTask[AnyRef, Int, Seq[String]] {
+class GoodsTask(subname: String, activity: Activity, goodsGrid: GridLayout) extends AsyncTask[AnyRef, Int, Seq[String]] {
   def doInBackground(params: AnyRef*) = {
     val goods = findGoods(subname)
     activity.runOnUiThread(new Runnable() {
       def run() {
-        val goodsAdapter = new ArrayAdapter[String](activity, R.layout.tile, new util.ArrayList[String]())
-        val layoutParams = new LinearLayout.LayoutParams(100, -1, 1F)
-        goodsListView.setLayoutParams(layoutParams)
-        goodsListView.setAdapter(goodsAdapter)
-        goodsAdapter.clear()
-        goodsAdapter.addAll(goods)
-        goodsAdapter.notifyDataSetChanged()
+        goods.foreach {
+          case good =>
+            val view: TextView = activity.getLayoutInflater.inflate(R.layout.tile, null).asInstanceOf[TextView]
+            view.setText(good)
+            view.setClickable(true)
+            view.setOnClickListener(new OnClickListener {
+              def onClick(v: View) {
+                val textView = v.asInstanceOf[TextView]
+                goodsGrid.removeAllViews()
+                new GoodsTask(textView.getText.toString, activity, goodsGrid).execute()
+              }
+            })
+            goodsGrid.addView(view)
+        }
       }
     })
     goods
@@ -81,7 +67,7 @@ class GoodsTask(subname: String, activity: Activity, goodsListView: ListView) ex
 
   private def mats(subnameLength: Int, subname: String, prevSize: Int): List[String] = {
     matsBy(subnameLength, subname) match {
-      case x if x.size < 10 && maxLength(x) > prevSize =>
+      case x if x.size < 15 && maxLength(x) > prevSize =>
         mats(subnameLength + 1, subname, maxLength(x))
       case x => x
     }
